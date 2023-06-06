a8e6936f04e43ddd8319d25a2e4ba94838b23274
Eugene_Pro <proskuryakov.e@list.ru>
2023-06-06 18:50:44 +0300

    обновлена функция удаления

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ def create_add_note() -> None:
     }
     save_notes(notes)
     print(f"Заметка с id {id_note} добавлена")
+    print()
 
 
 def get_notes() -> dict:
@@ -33,7 +34,7 @@ def get_notes() -> dict:
     return notes
 
 
-def save_notes(notes):
+def save_notes(notes) -> None:
     """Сохраняем словарь заметок в файл notes.json"""
     with open("notes.json", "w") as file:
         json.dump(notes, file, indent=2)
@@ -77,39 +78,44 @@ def update_note() -> None:
 
 def delete_note() -> None:
     """Удаляет заметку по идентификатору или заголовку заметки"""
-    delete_by = input("Удалить по идентификатору или по заголовку? Введите 'id' или 'title': ")
-    notes = get_notes()
-    if delete_by == "id":
-        id_note = input("Введите идентификатор заметки для удаления: ")
-        if id_note in notes:
-            del notes[id_note]
-            save_notes(notes)
-            print(f"Заметка с идентификатором {id_note} удалена")
-        else:
-            print(f"Заметка с идентификатором {id_note} не найдена")
-    elif delete_by == "name":
-        title = input("Введите заголовок заметки для удаления")
-        found_notes = []
-        for note in notes.values():
-            if note["title"] == title:
-                found_notes.append(note)
-        if found_notes:
-            print(f"Было найдено несколько заметок с заголовком {title}:")
-            for note in found_notes:
-                print(f"Идентификатор: {note['id']}")
-                print(f"Заголовок: {note['title']}")
-                print(f"Текст заметки: {note['body']}")
-                print(f"Дата создания: {note['date']}")
+    while True:
+        delete_by = input("Удалить по идентификатору или по заголовку? Введите 'id' или 'title': ")
+        notes = get_notes()
+        if delete_by == "id":
             id_note = input("Введите идентификатор заметки для удаления: ")
             if id_note in notes:
                 del notes[id_note]
                 save_notes(notes)
-                print(f"Заметка с идентификатором {id_note} и именем {title} удалена")
-                time.sleep(1)
-                print()
+                print(f"Заметка с идентификатором {id_note} удалена\n")
             else:
                 print(f"Заметка с идентификатором {id_note} не найдена")
+        elif delete_by == "name":
+            title = input("Введите заголовок заметки для удаления")
+            found_notes = []
+            for note in notes.values():
+                if note["title"] == title:
+                    found_notes.append(note)
+            if found_notes:
+                print(f"Было найдено несколько заметок с заголовком {title}:")
+                for note in found_notes:
+                    print(f"Идентификатор: {note['id']}")
+                    print(f"Заголовок: {note['title']}")
+                    print(f"Текст заметки: {note['body']}")
+                    print(f"Дата создания: {note['date']}")
+                id_note = input("Введите идентификатор заметки для удаления: ")
+                if id_note in notes:
+                    del notes[id_note]
+                    save_notes(notes)
+                    print(f"Заметка с идентификатором {id_note} и именем {title} удалена\n")
+                    print()
+                    time.sleep(1)
+                else:
+                    print(f"Заметка с идентификатором {id_note} не найдена")
+            else:
+                print(f"Заметка с именем {title} не найдена")
         else:
-            print(f"Заметка с именем {title} не найдена")
-    else:
-        print("Неизвестный параметр удаления. Введите 'id' или 'title'")
+            print("Неизвестный параметр удаления. Введите 'id' или 'title'")
+        choice = input("Продолжить удаление? Введите 'y' или 'n'\n>> ")
+        if choice.lower().strip() == 'n':
+            print()
+            break
